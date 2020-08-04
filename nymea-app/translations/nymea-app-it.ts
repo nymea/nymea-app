07651aa2b273103310b9de9480bd6bc06054f765
Michele <miguel2000@livecom.it>
2020-08-04 07:50:31 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (1225 of 1225 strings)
    
    Translation: Open Source apps/nymea:app
    Translate-URL: http://translate.nymea.io/projects/open-source-apps/nymea-app/it/

--- nymea-app/translations/nymea-app-it.ts
+++ nymea-app/translations/nymea-app-it.ts
@@ -151,7 +151,7 @@
     </message>
     <message>
         <source>Discovering services of %1...</source>
-        <translation>Scoprendo i servizi di %1...</translation>
+        <translation>Trovando servizi di %1...</translation>
     </message>
     <message>
         <source>%1 connected and discovered.</source>
