a5981e300cc1be2f64785bf0f4a860b7f793da97
musyafa <senbol@gmail.com>
2021-08-20 18:26:06 +0000

    Translated using Weblate (Turkish)
    Currently translated at 90.3% (1406 of 1557 strings)
    
    Translation: Open Source apps/nymea:app
    Translate-URL: http://translate.nymea.io/projects/open-source-apps/nymea-app/tr/

--- nymea-app/translations/nymea-app-tr.ts
+++ nymea-app/translations/nymea-app-tr.ts
@@ -3924,7 +3924,7 @@ Please use a timeout value bigger or equal to 10 ms.</source>
     <message>
         <source>There are no Modbus RTU masters set up yet.
 In order to have Modbus RTU available as resource in the system, please add a new Modbus RTU master.</source>
-        <translation>Henüz ayarlanmış bir Modbus RTU yönetici yok. Modbus RTU&apos;nun sistemde kaynak olarak kullanılabilir olması için lütfen yeni bir Modbus RTU yönetici ekleyin.</translation>
+        <translation>Henüz ayarlanmış bir Modbus RTU yöneticisi yok. Modbus RTU&apos;nun sistemde kaynak olarak kullanılabilir olması için lütfen yeni bir Modbus RTU yönetici ekleyin.</translation>
     </message>
     <message>
         <source>Modbus RTU is not supported on this platorm.</source>
