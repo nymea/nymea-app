bb5a86aa0144f254491366f408a85920a277eea4
Simon Stürz <simon.stuerz@nymea.io>
2026-03-20 15:23:54 +0100

    Implement backup file downloads

--- libnymea-app/configuration/nymeaconfiguration.h
+++ libnymea-app/configuration/nymeaconfiguration.h
@@ -121,6 +121,7 @@ public:
     Q_INVOKABLE void getBackupFiles();
     Q_INVOKABLE void createBackup();
     Q_INVOKABLE void createAndDownloadBackup();
+    Q_INVOKABLE void downloadBackupFile(const QString &fileName);
 
     void init();
 
@@ -134,6 +135,7 @@ private:
     Q_INVOKABLE void setBackupConfigurationResponse(int commandId, const QVariantMap &params);
     Q_INVOKABLE void createBackupReply(int commandId, const QVariantMap &params);
     Q_INVOKABLE void createAndDownloadBackupReply(int commandId, const QVariantMap &params);
+    Q_INVOKABLE void downloadBackupFileReply(int commandId, const QVariantMap &params);
     Q_INVOKABLE void setTcpConfigReply(int commandId, const QVariantMap &params);
     Q_INVOKABLE void deleteTcpConfigReply(int commandId, const QVariantMap &params);
     Q_INVOKABLE void setWebSocketConfigReply(int commandId, const QVariantMap &params);
@@ -160,6 +162,7 @@ signals:
     void setBackupConfigurationFinished(int commandId, const QString &configurationError);
     void createBackupFinished(int commandId, const QString &configurationError);
     void createAndDownloadBackupFinished(int commandId, const QString &configurationError, const QString &downloadId, const QString &fileName, int size);
+    void downloadBackupFileFinished(int commandId, const QString &configurationError, const QString &downloadId, const QString &fileName, int size);
 
 private:
     void updateBackupConfiguration(const QString &backupDestinationDirectory, int backupMaxCount);
